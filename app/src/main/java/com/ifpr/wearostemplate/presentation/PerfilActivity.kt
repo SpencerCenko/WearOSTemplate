e2fde1d0eb9addeefb805919e1fe5f46d3fd7dc8
pontagrossa-ai <pontagrossa@ifpr.edu.br>
2026-06-23 14:24:40 -0300

    perfil parcial

---
+++ app/src/main/java/com/ifpr/wearostemplate/presentation/PerfilActivity.kt
@@ -0,0 +1,21 @@
+package com.ifpr.wearostemplate.presentation
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.ifpr.wearostemplate.R
+
+class PerfilActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_perfil)
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }*/
+    }
+}
